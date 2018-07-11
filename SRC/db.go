ebaae48686668a665119ba1d68a94924ec5e3e30
EchoChang <cococo819@gmail.com>
2018-07-11 22:42:27 +0800

    All function enabled

--- SRC/db.go
+++ SRC/db.go
@@ -32,10 +32,10 @@ func getTrackingByUserID(t string, con *sql.DB) ([]Tracking, error) {
 	}
 
 	var ts []Tracking
-	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_TrackingByUserID] ?", t))
+	rows, err := con.Query("exec [dbo].[Select_TrackingByUserID] '" + t + "'")
 	for rows.Next() {
 		var t Tracking
-		err = rows.Scan(&t.TrackingID, &t.UserID, &t.SessionID, &t.Stage, &t.Msg, &t.LogTime, &t.QuestionID, &t.Answer)
+		err = rows.Scan(&t.TrackingID, &t.SessionID, &t.UserID, &t.LessionID, &t.Event, &t.Note, &t.LogUTCTime)
 		ts = append(ts, t)
 	}
 
@@ -43,6 +43,84 @@ func getTrackingByUserID(t string, con *sql.DB) ([]Tracking, error) {
 
 	return ts, err
 }
+
+func getTrackingByDeviceIDLessionID(deviceID string, lessionID string, con *sql.DB) ([]Tracking, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var ts []Tracking
+	
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_TrackingByDeviceIDLessionID] ?,?"), deviceID, lessionID)
+	for rows.Next() {
+		var t Tracking
+		err = rows.Scan(&t.TrackingID, &t.SessionID, &t.UserID, &t.LessionID, &t.Event, &t.Note, &t.LogUTCTime)
+		ts = append(ts, t)
+	}
+
+	defer rows.Close()
+
+	return ts, err
+}
+
+
+
+func getTrackingByUserIDSessionID(userid string, sessionid string, con *sql.DB) ([]Tracking, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var ts []Tracking
+	//fmt.Println(fmt.Sprintf("exec [dbo].[Select_TrackingByUserIDSessionID] ?,?"), userid, sessionid)
+	//TrackingID	XRequestID	SessionID	UserID	LessionID	Event	Note	LogUTCTime
+	//1178	430d3147-4032-4ffb-8c35-48ec2d9a5eed	20180704	105554002	0	USER_LOGGINGIN	User:105554002 Logging in at:0	2018-07-04 06:17:22.313
+	//TrackingID int
+	//XRequestID  string
+	//SessionID   string
+	//UserID     int
+	//LessionID  string
+	//Event      string
+	//Note        *string
+	//LogUTCTime    time.Time
+	
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_TrackingByUserIDSessionID] ?,?"), userid, sessionid)
+	for rows.Next() {
+		var t Tracking
+		err = rows.Scan(&t.TrackingID, &t.XRequestID, &t.SessionID, &t.UserID, &t.LessionID, &t.Event, &t.Note, &t.LogUTCTime)
+		ts = append(ts, t)
+	}
+
+	defer rows.Close()
+
+	return ts, err
+}
+
+func getProgressesByUserIDSessionID(userid int, sessionid string, con *sql.DB) ([]int, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	ps := make([]int,0)
+	
+	//fmt.Println(fmt.Sprintf("exec [dbo].[Select_ProgressByUserIDLessionID] ?,?"), userid, lessionid)
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_ProgressByUserIDSessionID] ?,?"), userid, sessionid)
+	for rows.Next() {
+		var p int
+		err = rows.Scan(&p)
+		ps = append(ps, p)
+	}
+
+	defer rows.Close()
+
+	return ps, err
+}
+
 
 func getQuestionByQuestionGroupID(t string, con *sql.DB) ([]Question, error) {
 
@@ -64,6 +142,191 @@ func getQuestionByQuestionGroupID(t string, con *sql.DB) ([]Question, error) {
 	return quizs, err
 }
 
+
+func getWordsBySemesterIDFromDB(t string, con *sql.DB) ([]Word, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var words []Word
+	rows, err := con.Query("exec [dbo].[Select_WordsBySemesterID] '" + t + "'")
+	for rows.Next() {
+		var wd Word
+		err = rows.Scan(&wd.ID, &wd.SemesterID, &wd.LessionID, &wd.ImagePath, &wd.AudioPath, &wd.Answer, &wd.Options)
+		words = append(words, wd)
+	}
+
+	defer rows.Close()
+
+	return words, err
+}
+
+func getOneWordByWordID(t int, con *sql.DB) ([]Word, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var words []Word
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_WordsByWordID] ?"), t)
+
+	//rows, err := con.Query("exec [dbo].[Select_WordsByWordID] '" + t + "'")
+	for rows.Next() {
+		var wd Word
+		err = rows.Scan(&wd.ID, &wd.SemesterID, &wd.LessionID, &wd.ImagePath, &wd.AudioPath, &wd.Answer, &wd.Options)
+		words = append(words, wd)
+	}
+
+	defer rows.Close()
+
+	return words, err
+}
+
+
+func getAnswerByUserIDSessionIDFromDB(userid int, sessionid string, con *sql.DB) ([]Answer, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var as []Answer
+	
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_AnswerByUserIDSessionID] ?,?"), userid, sessionid)
+	for rows.Next() {
+		var a Answer
+		err = rows.Scan(&a.AnswerID, &a.UserID, &a.SessionID, &a.SemesterID, &a.LessionID, &a.UserAnswer, &a.CorrectAnswer)
+		as = append(as, a)
+	}
+
+	defer rows.Close()
+
+	return as, err
+}
+
+func getComboFromDBBySessionID(sessionid string, con *sql.DB) ([]ComboRecord, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var cb []ComboRecord
+	
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_ComboBySessionID] ?"), sessionid)
+	for rows.Next() {
+		var c ComboRecord
+		err = rows.Scan(&c.UserName, &c.UserImgPath, &c.UserCombo)
+		cb = append(cb, c)
+	}
+
+	defer rows.Close()
+
+	return cb, err
+}
+
+
+func getPracticeByUserIDSessionIDFromDB(userid int, sessionid string, con *sql.DB) (Word, error) {
+
+	err := con.Ping()
+	var ww Word
+	if err != nil {
+		return ww, err
+	}
+
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_PracticeWordByUserIDSessionID] ?,?"), userid, sessionid)
+
+	for rows.Next() {
+		var wd Word 	
+		//fmt.Println(fmt.Sprintf("exec [dbo].[Select_PracticeWordByUserIDSessionID] ?,?"), userid, sessionid)
+
+		err = rows.Scan(&wd.ID, &wd.SemesterID, &wd.LessionID, &wd.ImagePath, &wd.AudioPath, &wd.Answer, &wd.Options)
+		ww = wd
+	}
+
+	defer rows.Close()
+
+	return ww, err
+}
+
+func GetStairRecordsFromDBBySessionID(sessionid string, con *sql.DB) ([]StairRecord, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	var sr []StairRecord
+	
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_StairBySessionID] ?"), sessionid)
+	for rows.Next() {
+		var s StairRecord
+		err = rows.Scan(&s.UserName, &s.LocalPhotoPath, &s.TotalCnt)
+		sr = append(sr, s)
+	}
+
+	defer rows.Close()
+
+	return sr, err
+}
+
+
+func getUserPracticeResultByUserIDSessionID(userid int, sessionid string, con *sql.DB) (PracticeResult, error) {
+
+	err := con.Ping()
+	var rt PracticeResult
+	var comboCount = 0
+	var tempComboCnt = 0
+	var previousIsCorrect = true
+	var cb = 0
+ 	if err != nil {
+		return rt, err
+	}
+		
+
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_PracticeResultByUserIDSessionID] ?,?"), userid, sessionid)
+	for rows.Next() {
+		var pr PracticeRecord 	
+//
+		err = rows.Scan(&pr.UserPracticeID, &pr.UserID, &pr.SessionID, &pr.SemesterID, &pr.UserAnswer, &pr.CorrectAnswer, &pr.CorrectCnt, &pr.CorrectRate, pr.ComboInDB)
+		rt.CorrectCnt = pr.CorrectCnt
+		rt.CorrectRate = pr.CorrectRate
+		cb = pr.ComboInDB
+		if pr.UserAnswer == pr.CorrectAnswer {
+			if previousIsCorrect {
+				tempComboCnt += 1
+			} else {
+				tempComboCnt = 1
+			}
+			if tempComboCnt >= comboCount {
+				comboCount = tempComboCnt
+			}
+			previousIsCorrect = true
+		} else {
+			tempComboCnt = 0
+			previousIsCorrect = false
+		}
+			
+	}
+
+	
+	
+	rt.ComboCnt = comboCount
+	if comboCount > cb {
+			_, err = con.Exec(fmt.Sprintf("exec [dbo].[Upsert_ComboByUserIDComboCnt] ?,?,?"), userid,sessionid, comboCount)
+	}
+	defer rows.Close()
+//fmt.Println(fmt.Sprintf("exec [dbo].[Select_PracticeResultByUserIDSessionID] ?,?"), userid, sessionid)
+
+	return rt, err
+}
+
+
+
+
 func insertQuestion(q Question, con *sql.DB) (sql.Result, error) {
 
 	err := con.Ping()
@@ -76,6 +339,40 @@ func insertQuestion(q Question, con *sql.DB) (sql.Result, error) {
 	return r, err
 }
 
+func upsertAnswerToDB(a Answer,con *sql.DB)  (sql.Result, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+	//exec [dbo].[Upsert_Answer] '105554002','20180704',3,1,'qqq','ccc'
+	//AsnwerID	int
+    //UserID		int
+    //SessionID	int
+    //SemesterID	int
+    //LessionID	int
+    //UserAnswer	string
+    //orrectAnswer string
+	
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Upsert_Answer] ?,?,?,?,?,?"), a.UserID, a.SessionID, a.SemesterID, a.LessionID, a.UserAnswer, a.CorrectAnswer)
+
+	return r, err
+}
+
+
+
+func insertOneUser(u User, con *sql.DB) (sql.Result, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_User] ?,?,?,?"), u.UserID, u.UserName, u.PhotoPath, u.LocalPhotoPath)
+
+	return r, err
+}
+
 func insertTracking(t Tracking, con *sql.DB) (sql.Result, error) {
 
 	err := con.Ping()
@@ -83,7 +380,64 @@ func insertTracking(t Tracking, con *sql.DB) (sql.Result, error) {
 		return nil, err
 	}
 
-	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_Tracking] ?,?,?,?,?,?"), t.UserID, t.SessionID, t.Stage, t.Msg, t.QuestionID, t.Answer)
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_Tracking] ?,?,?,?,?,?"), t.XRequestID, t.SessionID, t.UserID, t.LessionID, t.Event, t.Note)
 
 	return r, err
+}
+
+func insertProgress(t Progress, con *sql.DB) (sql.Result, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_Progress] ?,?,?"), t.UserID, t.SessionID, t.LessionID)
+
+	return r, err
+}
+
+func insertUserPractice(p PracticeRecord, con *sql.DB) (sql.Result, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_PracticeRecord] ?,?,?,?,?"), p.UserID, p.SessionID, p.SemesterID, p.UserAnswer, p.CorrectAnswer)
+
+	return r, err
+}
+
+func insertWord(wd Word, con *sql.DB) (sql.Result, error) {
+
+	err := con.Ping()
+	if err != nil {
+		return nil, err
+	}
+
+	r, err := con.Exec(fmt.Sprintf("exec [dbo].[Insert_Word] ?,?,?,?,?,?"), wd.SemesterID, wd.LessionID, wd.ImagePath, wd.AudioPath, wd.Answer, wd.Options)
+
+	return r, err
+}
+
+
+func getUserInfoByUserID(t string, con *sql.DB) (User, error) {
+
+	var ts User
+	err := con.Ping()
+	if err != nil {
+		return ts, err
+	}
+
+	rows, err := con.Query(fmt.Sprintf("exec [dbo].[Select_UserByUserID] ?"), t)
+	for rows.Next() {
+		var t User
+		err = rows.Scan(&t.UserID, &t.UserName, &t.PhotoPath)
+		ts = t
+	}
+
+	defer rows.Close()
+
+	return ts, err
 }
--- SRC/main.go
+++ SRC/main.go
@@ -9,7 +9,7 @@ import (
 	"net/http"
 	"os"
 	"time"
-
+	"strconv"
 	_ "github.com/denisenkom/go-mssqldb"
 	"github.com/gorilla/mux"
 )
@@ -30,13 +30,112 @@ type Question struct {
 }
 type Tracking struct {
 	TrackingID int
+	XRequestID  string
+	SessionID   string
 	UserID     int
-	SessionID  string
-	Stage      string
-	Msg        *string
-	LogTime    time.Time
-	QuestionID int
-	Answer     string
+	LessionID  string
+	Event      string
+	Note        *string
+	LogUTCTime    time.Time
+}
+
+type ComboResult struct {
+	ComboRecords []ComboRecord
+}
+
+type ComboRecord struct {
+	UserName string
+	UserImgPath string
+	UserCombo int
+}
+
+type Textbook struct {
+	Words []Word
+}
+
+type Word struct {
+	ID int
+	SemesterID int
+	LessionID  int
+	ImagePath string
+	AudioPath string
+	Answer      string
+	Options        string
+}
+
+type Practice struct {
+
+	ID int
+	SemesterID int
+	LessionID  int
+	ImagePath string
+	AudioPath string
+	Answer      string
+	Options        string
+	CorrectCnt float32
+	CorrectRate float32
+	ComboCnt int
+}
+
+type PracticeResult struct {
+	CorrectCnt float32
+	CorrectRate float32
+	ComboCnt int
+
+}
+type PracticeRecord struct {
+	UserPracticeID int
+	UserID int
+	SessionID string
+	SemesterID int
+	UserAnswer string
+	CorrectAnswer string
+	CorrectCnt float32
+	CorrectRate float32
+	ComboInDB int
+	
+}
+	
+
+type Progress struct {
+	ProgressID 	int
+	UserID 		int
+	SessionID	string
+	LessionID	int
+}
+
+type CurrentProgress  struct {
+ Lessions []int `json: weather`
+ }
+ 
+type User struct {
+	UserID          int
+	UserName        string
+	PhotoPath       string	
+	LocalPhotoPath       string
+
+}
+
+type AnswerSet struct {
+	Answers []Answer
+}
+type Answer struct {
+	AnswerID	int
+    UserID		int
+    SessionID	string
+    SemesterID	int
+    LessionID	int
+    UserAnswer	string
+    CorrectAnswer string
+}
+
+type StairResult struct {
+	StairRecords []StairRecord
+}
+type StairRecord struct {
+	UserName string
+	LocalPhotoPath string
+	TotalCnt int
 }
 
 func init() {
@@ -53,18 +152,60 @@ func main() {
 	}
 	sqlcon, err = newConnection(c.DBUser, c.DBPasswd, c.DBDatabase, c.DBHost, c.DBPort)
 	if err != nil {
+		fmt.Println("[Error] error: ", err)
 		os.Exit(1)
 	}
 	err = sqlcon.Ping()
 	if err != nil {
+		fmt.Println("[Error] error: ", err)
 		os.Exit(1)
 	}
 
 	router := mux.NewRouter()
 	router.HandleFunc("/question/{groupid}", GetQuestions).Methods("GET")
-	router.HandleFunc("/tracking/{userid}", GetTrackings).Methods("GET")
+	router.HandleFunc("/user/{userid}", GetUserInfo).Methods("GET")
+	router.HandleFunc("/combo/{sessionid}", GetComboDataBySessionID).Methods("GET")	
+	router.HandleFunc("/stair/{sessionid}", GetStairResultBySessionID).Methods("GET")
+	router.HandleFunc("/word/{id}", GetOneWord).Methods("GET")
+
+	
+
+	
+	//router.HandleFunc("/tracking/{lessionID}/{deviceID}", GetTrackingsByDeviceIDLessionID).Methods("GET")
+	//router.Queries("deviceID")
+	
+	router.HandleFunc("/tracking/{userid}/{sessionid}", GetTrackingsByUserIDSessionID).Methods("GET")
+	router.Queries("sessionid")
+	
+	router.Handle("/photo/{userid}", http.StripPrefix("/photo/", http.FileServer(http.Dir("photo"))))
+    
+	router.Handle("/texture/{semester}/{lession}/{name}", http.StripPrefix("/texture/", http.FileServer(http.Dir("texture"))))
+    
+	
 	router.HandleFunc("/question", CreateQuestion).Methods("POST")
 	router.HandleFunc("/tracking", CreateTracking).Methods("POST")
+	router.HandleFunc("/answer", UpsertAnswer).Methods("POST")
+	router.HandleFunc("/practice", CreateUserPracticeRecord).Methods("POST")
+	
+	router.HandleFunc("/progress", CreateProgress).Methods("POST")
+	router.HandleFunc("/progress/{userid}/{sessionid}", GetProgressByUserIDSessionID).Methods("GET")
+	router.Queries("sessionid")
+	
+	//words
+	router.HandleFunc("/words/{semesterid}", GetWordsBySemesterID).Methods("GET")
+	router.HandleFunc("/word", CreateWord).Methods("POST")
+	
+	router.HandleFunc("/user", CreateUser).Methods("POST")
+	
+	//answer	
+	router.HandleFunc("/answer/{userid}/{sessionid}", GetAnswerByUserIDSessionID).Methods("GET")
+	router.Queries("sessionid")
+
+	//practice
+	
+	router.HandleFunc("/practice/{userid}/{sessionid}", GetPracticeByUserIDSessionID).Methods("GET")
+	router.Queries("sessionid")
+	
 	log.Fatal(http.ListenAndServe(":8000", router))
 
 }
@@ -74,6 +215,90 @@ func GetQuestions(w http.ResponseWriter, r *http.Request) {
 	quizs, _ := getQuestionByQuestionGroupID(params["groupid"], sqlcon)
 	json.NewEncoder(w).Encode(quizs)
 }
+
+func GetComboDataBySessionID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	
+    output := ComboResult{}	
+	result, _ := getComboFromDBBySessionID(params["sessionid"], sqlcon)
+
+	output.ComboRecords = result
+	
+	json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+	
+}
+func GetStairResultBySessionID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	
+    output := StairResult{}	
+	result, _ := GetStairRecordsFromDBBySessionID(params["sessionid"], sqlcon)
+
+	output.StairRecords = result
+	
+	json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+	
+}
+func GetOneWord(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	wid,_:=strconv.Atoi(params["id"]) 
+
+	words, _ := getOneWordByWordID(wid, sqlcon)
+	
+	
+    output := Textbook{}
+	output.Words = words
+	
+	json2, _ := json.Marshal(output)
+
+	
+	//output := CurrentProgress{}
+	//output.Lessions = progress
+	
+	//json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+	
+	//json.NewEncoder(w).Encode(json2)
+	
+}
+ 
+
+
+func GetWordsBySemesterID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	words, _ := getWordsBySemesterIDFromDB(params["semesterid"], sqlcon)
+	
+	
+    output := Textbook{}
+	output.Words = words
+	
+	json2, _ := json.Marshal(output)
+
+	
+	//output := CurrentProgress{}
+	//output.Lessions = progress
+	
+	//json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+	
+	//json.NewEncoder(w).Encode(json2)
+}
+
+func GetUserInfo(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	userInfo, _ := getUserInfoByUserID(params["userid"], sqlcon)
+	json.NewEncoder(w).Encode(userInfo)
+}
+
 
 func CreateQuestion(w http.ResponseWriter, r *http.Request) {
 
@@ -92,11 +317,173 @@ func CreateQuestion(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode(quiz)
 }
 
+
+func CreateUser(w http.ResponseWriter, r *http.Request) {
+
+	var usrs []User
+	json.NewDecoder(r.Body).Decode(&usrs)
+	for _, v := range usrs{
+		result, err := insertOneUser(v, sqlcon)
+		switch err {
+		case sql.ErrNoRows:
+			fmt.Println("No rows were returned!")
+			return
+		case nil:
+			fmt.Println(result)
+		default:
+			panic(err)
+		}
+	}
+//	json.NewEncoder(w).Encode(usrs)
+}
+
 func GetTrackings(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
 	trackings, _ := getTrackingByUserID(params["userid"], sqlcon)
 	json.NewEncoder(w).Encode(trackings)
 }
+func CreateProgress(w http.ResponseWriter, r *http.Request) {
+
+	var p Progress
+	json.NewDecoder(r.Body).Decode(&p)
+	result, err := insertProgress(p, sqlcon)
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return
+	case nil:
+		fmt.Println(result)
+	default:
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(p)
+}
+
+func CreateUserPracticeRecord(w http.ResponseWriter, r *http.Request) {
+
+	var p PracticeRecord
+	json.NewDecoder(r.Body).Decode(&p)
+	result, err := insertUserPractice(p, sqlcon)
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return
+	case nil:
+		fmt.Println(result)
+	default:
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(p)
+}
+
+func CreateWord(w http.ResponseWriter, r *http.Request) {
+
+	var wds []Word
+	json.NewDecoder(r.Body).Decode(&wds)
+	
+	for _, v := range wds{
+		result, err := insertWord(v, sqlcon)
+		switch err {
+		case sql.ErrNoRows:
+			fmt.Println("No rows were returned!")
+			return
+		case nil:
+			fmt.Println(result)
+		default:
+			panic(err)
+		}
+	}
+	
+	json.NewEncoder(w).Encode(wds)
+}
+
+func GetTrackingsByDeviceIDLessionID(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	trackings, _ := getTrackingByDeviceIDLessionID(params["deviceID"], params["lessionID"], sqlcon)
+	json.NewEncoder(w).Encode(trackings)
+}
+
+func GetTrackingsByUserIDSessionID(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Println("No rows were returned!")
+	params := mux.Vars(r)
+	//uid,_:=strconv.Atoi(params["userid"]) 
+	//fmt.Println(uid)
+	//fmt.Println( params["sessionid"])
+	
+	trackings, _ := getTrackingByUserIDSessionID(params["userid"], params["sessionid"], sqlcon)
+	json.NewEncoder(w).Encode(trackings)
+}
+
+func GetProgressByUserIDSessionID(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Println("No rows were returned!")
+	params := mux.Vars(r)
+	uid,_:=strconv.Atoi(params["userid"]) 
+	//fmt.Println(uid)
+	
+	progress, _ := getProgressesByUserIDSessionID(uid, params["sessionid"], sqlcon)
+	
+	output := CurrentProgress{}
+	output.Lessions = progress
+	
+	json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+}
+
+func GetAnswerByUserIDSessionID(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Println("No rows were returned!")
+	params := mux.Vars(r)
+	uid,_:=strconv.Atoi(params["userid"]) 
+	//fmt.Println(uid)
+	
+	as, _ := getAnswerByUserIDSessionIDFromDB(uid, params["sessionid"], sqlcon)
+	
+	output := AnswerSet{}
+	output.Answers = as
+	
+	json2, _ := json.Marshal(output)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+}
+func GetPracticeByUserIDSessionID(w http.ResponseWriter, r *http.Request) {
+
+	//fmt.Println("No rows were returned!")
+	params := mux.Vars(r)
+	uid,_:=strconv.Atoi(params["userid"]) 
+	//fmt.Println(uid)
+	
+	wd, _ := getPracticeByUserIDSessionIDFromDB(uid, params["sessionid"], sqlcon)
+	
+	result, _ := getUserPracticeResultByUserIDSessionID(uid, params["sessionid"], sqlcon)
+	
+	
+	pp := Practice{}
+	pp.ID = wd.ID
+	pp.SemesterID = wd.SemesterID
+	pp.LessionID = wd.LessionID
+	pp.ImagePath = wd.ImagePath
+	pp.AudioPath = wd.AudioPath
+	pp.Answer = wd.Answer
+	pp.Options = wd.Options
+	
+
+	pp.CorrectCnt = result.CorrectCnt
+	pp.CorrectRate = result.CorrectRate
+	pp.ComboCnt = result.ComboCnt
+	
+	fmt.Println(pp.AudioPath)
+	
+	json2, _ := json.Marshal(pp)
+	w.Header().Set("Content-Type","application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(json2)
+}
+
 
 func CreateTracking(w http.ResponseWriter, r *http.Request) {
 
@@ -113,4 +500,21 @@ func CreateTracking(w http.ResponseWriter, r *http.Request) {
 		panic(err)
 	}
 	json.NewEncoder(w).Encode(t)
+}
+
+func UpsertAnswer(w http.ResponseWriter, r *http.Request) {
+
+	var a Answer
+	json.NewDecoder(r.Body).Decode(&a)
+	result, err := upsertAnswerToDB(a, sqlcon)
+	switch err {
+	case sql.ErrNoRows:
+		fmt.Println("No rows were returned!")
+		return
+	case nil:
+		fmt.Println(result)
+	default:
+		panic(err)
+	}
+	json.NewEncoder(w).Encode(a)
 }
